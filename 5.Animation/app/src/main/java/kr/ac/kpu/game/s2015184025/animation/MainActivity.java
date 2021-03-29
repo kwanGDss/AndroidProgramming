84120edcc928b260be119ccb3de38958bb3bbb8c
kwanGDss <jky6216@naver.com>
2021-03-29 10:49:58 +0900

    Add Animation solution
    Add Animation solution

---
+++ 5.Animation/app/src/main/java/kr/ac/kpu/game/s2015184025/animation/MainActivity.java
@@ -0,0 +1,17 @@
+package kr.ac.kpu.game.s2015184025.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //MyView mv = new MyView(this, null);
+        //setContentView(mv);
+    }
+}
---
+++ 5.Animation/app/src/main/java/kr/ac/kpu/game/s2015184025/animation/MyView.java
@@ -0,0 +1,39 @@
+package kr.ac.kpu.game.s2015184025.animation;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyView extends View {
+    private static final String TAG = MyView.class.getSimpleName();
+    private Paint paint = new Paint();
+    private Rect rect = new Rect();
+
+    public MyView(Context context, AttributeSet set) {
+        super(context, set);
+        paint.setColor(0xff0044ff);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouch" + event);
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int l = 0 + getLeftPaddingOffset();
+        int t = 0 + getTopPaddingOffset();
+        int w = getWidth() - getRightPaddingOffset();
+        int h = getHeight() - getBottomPaddingOffset();
+        rect.set(l, t, w, h);
+        Log.d(TAG, "drawing" + rect);
+        canvas.drawRect(rect, paint);
+    }
+}
