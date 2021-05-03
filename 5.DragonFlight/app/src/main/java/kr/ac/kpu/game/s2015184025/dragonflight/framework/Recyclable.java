c0e6d03c875b3713a7fad91b072e9f177cc8b7d4
kwanGDss <66257657+kwanGDss@users.noreply.github.com>
2021-05-03 10:56:20 +0900

    Add DragonFlight

---
+++ 5.DragonFlight/app/src/main/java/kr/ac/kpu/game/s2015184025/dragonflight/framework/Recyclable.java
@@ -0,0 +1,5 @@
+package kr.ac.kpu.game.s2015184025.dragonflight.framework;
+
+public interface Recyclable {
+    public void recycle();
+}
---
+++ 5.DragonFlight/app/src/main/java/kr/ac/kpu/game/s2015184025/dragonflight/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.kpu.game.s2015184025.dragonflight.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.ac.kpu.game.s2015184025.dragonflight.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
